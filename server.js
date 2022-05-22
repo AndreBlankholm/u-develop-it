14fb39cd608ff54c157875b19e0b54794be788e1
AndreBlankholm <ablankholm@gmail.com>
2022-05-21 23:52:22 -0500

    Crud operations in MYSQL

--- server.js
+++ server.js
@@ -26,18 +26,50 @@ const db = mysql.createConnection(
 );
 
 
+//Database call that returns all rows in SELECTED * FROM
+//db.query(`SELECT * FROM candidates`, (err, rows) => {
+  //  console.log(rows);
+  //});
 
-db.query(`SELECT * FROM candidates`, (err, rows) => {
-console.log(rows);
-});
+
+// GET a single candidate 1 is a placeholder
+//db.query(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
+   // if (err) {
+   //   console.log(err);
+   // }
+   // console.log(row);
+ // });
+
+//Delete a candidate 1 is a placeholder
+//db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
+    //if (err) {
+        //console.log(err);
+   // }
+   // console.log(result);
+//});
+
+
+//Create a new candidate
+//const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected) 
+              //VALUES (?,?,?,?)`;
+//const params = [1, 'Ronald', 'Firbank', 1];
+
+//db.query(sql, params, (err, result) => {
+  //if (err) {
+    //console.log(err);
+  //}
+  //console.log(result);
+//});
+
 
 
 //route to handle user requests that arent supported by my app !! Make sure this is the last route always
 app.use((req, res) => {
- res.status(404).end();
-});
+    res.status(404).end();
+  });
 
 //add the fuction that will start the server and tell you in console log
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
+ 
