56100517c6922399d7cc52ff32edb108450ab0d2
AndreBlankholm <ablankholm@gmail.com>
2022-05-24 20:13:45 -0500

    get vote count api call

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
-
+//the api routes passing through the index file here
+router.use(require('./voteRoute'));
 router.use(require('./voterRoutes'));
 router.use(require('./candidateRoutes'));
 
